c53cd9bcc4ffcdb4f3b8c26bfe0fa35c09618599
Chudi Nnorukam <chudinnorukam@Chudis-Air.attlocal.net>
2025-07-20 01:31:01 -0700

    Add simple test page and vercel.json config

---
+++ src/app/simple/page.tsx
@@ -0,0 +1,9 @@
+export default function SimplePage() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Simple Test Page</h1>
+      <p>This is a simple test page without any dependencies.</p>
+      <p>If you can see this, the basic build process is working.</p>
+    </div>
+  );
+} 
